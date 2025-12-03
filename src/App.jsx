a40ad245ed403d094255b7df0e1f470c3690ade7
Filip Nikolin <f.nikolin997@gmail.com>
2025-12-03 21:43:33 +0100

    Integrate WebRTC getUserMedia API, add initial logic for showing camera view, add initial html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [stream,setStream] = useState(null);
+const [countdown, setCountdown] = useState(null);
+
+const videoRef = useRef(null);
+const canvasRef = useRef(null);
+
+const startCamera = async() => {
+
+  try {
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: {width:1280, height:720},
+      audio:false
+    });
+    setStream(mediaStream);
+
+    if(videoRef.current) {
+      videoRef.current.srcObject = mediaStream;
+
+      await new Promise((resolve) => {
+        if(videoRef.current) {
+          videoRef.current.onloadedmetadata = () => {
+            videoRef.current?.play();
+            resolve();
+          }
+        }
+      })
+    }
+
+    let timeLeft = 5;
+    setCountdown(timeLeft);
+    const countdownInterval = setInterval(()=> {
+      timeLeft -= 1;
+      setCountdown(timeLeft);
+      if(timeLeft === 0) {
+        clearInterval(countdownInterval);
+        capturePhoto();
+      }
+    },1000);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const capturePhoto = () => {
+  // save photo and show to the UI
+  console.log('photo taken')
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={startCamera}>Take a photo</button>
+      {stream && (
+        <div>
+          <video ref={videoRef} autoPlay playsInline muted/>
+        </div>
+)}
+    countdown: {countdown}
+    <canvas ref={canvasRef}></canvas>
+    </div>
   )
 }
 
