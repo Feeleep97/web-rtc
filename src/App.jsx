40279be1be8363cdd1d5ac5a066d69ac41d56353
Filip Nikolin <f.nikolin997@gmail.com>
2025-12-04 18:24:28 +0100

    Resolve issue with camera not loading on first button click, resolve issue with multiple images on button click, remove excess code

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,15 @@
-import {useRef, useState } from 'react'
+import {useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
 const [stream,setStream] = useState(null);
 const [countdown, setCountdown] = useState(null);
-const [capturedImage, setCapturedImage] = useState(null);
 const [errorMessage,setErrorMessage] = useState('');
 
 const videoRef = useRef(null);
 const canvasRef = useRef(null);
 
-const startCamera = async() => {
-
-  try {
-    const mediaStream = await navigator.mediaDevices.getUserMedia({
-      video: {width:1280, height:720},
-      audio:false
-    });
-    setStream(mediaStream);
-
-    if(videoRef.current) {
-      videoRef.current.srcObject = mediaStream;
-
-      await new Promise((resolve) => {
-        if(videoRef.current) {
-          videoRef.current.onloadedmetadata = () => {
-            videoRef.current?.play();
-            resolve();
-          }
-        }
-      })
-    }
-
-    let timeLeft = 5;
-    setCountdown(timeLeft);
-    const countdownInterval = setInterval(()=> {
-      timeLeft -= 1;
-      setCountdown(timeLeft);
-      if(timeLeft === 0) {
-        clearInterval(countdownInterval);
-        capturePhoto();
-      }
-    },1000);
-  } catch (error) {
-    if(error.name === 'NotAllowedError' || error.name === 'PermissionDeniedError') {
-      setErrorMessage('Camera permission denied. Please allow camera permissions in order to take a photo.')
-    } else {
-      setErrorMessage(`Error accessing camera:${error.message}`)
-    }
-  }
-}
-
 const capturePhoto = () => {
-  // save photo and show to the UI
   if(videoRef.current && canvasRef.current) {
     const canvas = canvasRef.current;
     const video = videoRef.current;
@@ -63,28 +20,63 @@ const capturePhoto = () => {
     const ctx = canvas.getContext('2d');
     if(ctx) {
       ctx.drawImage(video,0,0,canvas.width,canvas.height);
-      const imageDataUrl = canvas.toDataURL('image/png')
-      setCapturedImage(imageDataUrl)
+      videoRef.current.srcObject.getTracks().forEach(track => track.stop());
+      setStream(false);
     }
   }
 }
 
+useEffect(() => {
+  const startCamera = async() => {
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: {width:1280, height:720},
+        audio:false
+      });
+      if(videoRef.current) {
+        videoRef.current.srcObject = mediaStream;
+  
+        await new Promise((resolve) => {
+          if(videoRef.current) {
+            videoRef.current.onloadedmetadata = () => {
+              videoRef.current?.play();
+              resolve();
+            }
+          }
+        })
+      }
+  
+      let timeLeft = 5;
+      setCountdown(timeLeft);
+      const countdownInterval = setInterval(()=> {
+        timeLeft -= 1;
+        setCountdown(timeLeft);
+        if(timeLeft === 0) {
+          clearInterval(countdownInterval);
+          capturePhoto();
+        }
+      },1000);
+    } catch (error) {
+      if(error.name === 'NotAllowedError' || error.name === 'PermissionDeniedError') {
+        setErrorMessage('Camera permission denied. Please allow camera permissions in order to take a photo.')
+      } else {
+        setErrorMessage(`Error accessing camera:${error.message}`)
+      }
+    }
+  }
+  if(stream) startCamera();
+},[stream])
+
+const handleStart = () => setStream(true);
+
   return (
     <div>
-      <button onClick={startCamera}>Take a photo</button>
+      <button onClick={handleStart}>Start</button>
       {stream && (
-        <div>
-          <video ref={videoRef} autoPlay playsInline muted/>
-        </div>
-)}
+        <video ref={videoRef} autoPlay playsInline muted />
+      )}
     countdown: {countdown}
-    <canvas ref={canvasRef}></canvas>
-
-    {capturedImage && (
-    <div>
-      <img src={capturedImage} alt="selfie" />
-    </div>
-)}
+    <canvas ref={canvasRef} />
     {errorMessage && (
       <div>Error message: {errorMessage}</div>
     )}
